634f308e85e07a382df3aeec5860d8061741efdf
Marcin Moskała <marcinmoskala@gmail.com>
2023-06-20 20:58:05 +0200

    Update lib versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,13 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.22"
-    id("com.google.devtools.ksp") version "1.7.22-1.0.8"
+    kotlin("jvm") version "1.8.10"
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9"
     application
 }
 
 group = "academy.kt"
 version = "1.0-SNAPSHOT"
-
-// Makes generated code visible to IDE
-kotlin.sourceSets.main {
-    kotlin.srcDirs(
-        file("$buildDir/generated/ksp/main/kotlin"),
-    )
-}
 
 dependencies {
     implementation(project(":generateinterface-annotations"))
--- generateinterface-processor/build.gradle.kts
+++ generateinterface-processor/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.22"
+    kotlin("jvm") version "1.8.10"
 }
 
 group = "academy.kt"
@@ -11,7 +11,7 @@ dependencies {
     implementation("com.squareup:kotlinpoet:1.12.0")
     implementation("com.squareup:kotlinpoet-ksp:1.12.0")
     implementation(project(":generateinterface-annotations"))
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.6.20-1.0.5")
+    implementation("com.google.devtools.ksp:symbol-processing-api:1.8.10-1.0.9")
     testImplementation(kotlin("test"))
     testImplementation("com.github.tschuchortdev:kotlin-compile-testing:1.4.9")
     testImplementation("com.github.tschuchortdev:kotlin-compile-testing-ksp:1.4.9")
